74cf8ed11cf071e7d0c688dd2aedb926788dfb4e
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-05-17 20:58:37 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,17 +4,18 @@
 //   metaMaskWallet,
 //   coinbaseWallet,
 // } from "@rainbow-me/rainbowkit/wallets";
-import { createPublicClient, defineChain, getContract, http } from "viem";
+import { createPublicClient, defineChain, getContract, http,
+} from "viem";
 import {
   base,
   baseSepolia,
   mainnet,
   optimism,
   optimismSepolia,
-  sepolia,
+  sepolia, 
 } from "viem/chains";
 import { createConfig } from "wagmi";
-import { erc20Abi, optimismPortalAbi } from "./abi";
+import { erc20Abi, optimismPortalAbi, l1tol2mesAbi } from "./abi";
 
 import type { PublicL1ClientWithChain, PublicL2ClientWithChain } from "./types";
 import { chainConfig, publicActionsL1, publicActionsL2 } from "viem/op-stack";
@@ -29,6 +30,32 @@ import { chainConfig, publicActionsL1, publicActionsL2 } from "viem/op-stack";
  * TODO:
  * - load walletConnect project id from the environment:
  */
+//
+const JibchainTestnet = defineChain({
+  // ...chainConfig,
+  id: 88991,
+  name: 'JibchainTestnet',
+  nativeCurrency: {
+      decimals: 18,
+      name: 'tJBC',
+      symbol: 'tJBC',
+  },
+  rpcUrls: {
+      default: {
+          http: ['https://rpc.testnet.jibchain.net'],
+          webSocket: [''],
+      },
+  },
+  blockExplorers: {
+      default: { name: 'Explorer', url: 'https://exp.testnet.jibchain.net/' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xa1a858ad9041B4741e620355a3F96B3c78e70ecE',
+      blockCreated: 32848,
+    },
+  },
+});
 
 const getChain = (chainId: number) => {
   const supportedChains = [
@@ -37,7 +64,8 @@ const getChain = (chainId: number) => {
     sepolia,
     optimism,
     baseSepolia,
-    optimismSepolia,
+    optimismSepolia, 
+    JibchainTestnet,
   ];
   const detectedChain = supportedChains.find((chain) => chain.id === chainId);
   if (!detectedChain) {
@@ -51,6 +79,7 @@ export const parentChain = getChain(Number(import.meta.env.VITE_L1_CHAIN_ID));
 const rpcUrls: string[] = import.meta.env.VITE_L2_RPC_URLS.split(",");
 
 export const rollupChain = defineChain({
+  ...chainConfig,
   id: Number(import.meta.env.VITE_L2_CHAIN_ID),
   name: import.meta.env.VITE_L2_CHAIN_NAME,
   nativeCurrency: {
@@ -73,14 +102,19 @@ export const rollupChain = defineChain({
     ...chainConfig.contracts,
     l2OutputOracle: {
       [parentChain.id]: {
-        address: import.meta.env.VITE_L2_OUTPUT_ORACLE_ADDRESS,
+        address: '0x74Ad6E0FB793eB5e6c1ff1225B03F5C5fFB7EF0c',
       },
     },
     portal: {
       [parentChain.id]: {
-        address: import.meta.env.VITE_L1_OPTIMISM_PORTAL_ADDRESS,
+        address: '0x0d605bb7d4FB586eAB750205F5247825F4D8AF4B',
       },
     },
+    // multicall3: {
+    //   [parentChain.id]: {
+    //     address: '0xa1a858ad9041B4741e620355a3F96B3c78e70ecE',
+    //   },
+    // },
   },
 });
 
@@ -97,8 +131,10 @@ export const rollupChain = defineChain({
 //   }
 // );
 
+
+
 export const parentClient = createPublicClient({
-  chain: baseSepolia,
+  chain: JibchainTestnet,
   transport: http(),
 }).extend(publicActionsL1()) as PublicL1ClientWithChain;
 
@@ -106,6 +142,20 @@ export const rollupClient = createPublicClient({
   chain: rollupChain,
   transport: http(),
 }).extend(publicActionsL2()) as PublicL2ClientWithChain;
+
+export const L1ToL2Mes = getContract({
+  address: import.meta.env.VITE_L1_TO_L2_MES_ADDRESS,
+  abi: l1tol2mesAbi,
+  client: rollupClient,
+});
+
+// export const l2native = getContract({
+//   address: import.meta.env.VITE_L1_L2_NATIVE,
+//   abi: erc20Abi,
+//   client: rollupClient,
+// });
+
+
 
 export const token = getContract({
   address: import.meta.env.VITE_L1_CUSTOM_GAS_TOKEN_ADDRESS,
@@ -118,6 +168,14 @@ export const optimismPortal = getContract({
   abi: optimismPortalAbi,
   client: parentClient,
 });
+
+
+
+// export const L1ToL2Mes = getContract({
+//   address: import.meta.env.VITE_L1_TO_L2_MES_ADDRESS,
+//   abi: l1tol2mesAbi,
+//   client: parentClient,
+// });
 
 export const config = createConfig({
   // connectors: connectors,
