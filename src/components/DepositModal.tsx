cbd5c3a466cb137bc864ab76c962cfdd1a3ab883
Stephen Caudill <hello@stephencaudill.com>
2024-06-13 13:19:22 -0600

    Mobile responsive layout for deposit modal

--- src/components/DepositModal.tsx
+++ src/components/DepositModal.tsx
@@ -39,30 +39,41 @@ export const DepositModal = ({
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-75" />
-      <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg flex flex-col gap-8 bg-base text-foreground">
-        <div className="flex flex-row justify-between items-start">
-          <Dialog.Title className="font-semibold text-2xl">
-            Deposit
-          </Dialog.Title>
-          <Dialog.Close>
-            <CloseIcon />
-          </Dialog.Close>
-        </div>
-        <div className="w-96 flex flex-col gap-6">
-          <DetailRow label="Routing" value="Standard Speed" />
-          <DetailRow
-            label="Amount of deposit"
-            value={`${formatEther(amount)} ${
-              rollupChain.nativeCurrency.symbol
-            }`}
-          />
-          <DetailRow
-            label="Gas fee"
-            value={`${formatEther(gasEstimate)} ${
-              parentChain.nativeCurrency.symbol
-            }`}
-          />
-          <DetailRow label="Time to transfer" value="~1 minute" />
+      <Dialog.Content className="fixed inset-0 flex justify-center items-center">
+        <div
+          className={clsx(
+            "p-8 lg:rounded-lg flex flex-col gap-8 bg-base text-foreground fixed lg:relative",
+            "top-0 bottom-0 left-0 right-0",
+            "mx-auto min-w-96 justify-between"
+          )}
+        >
+          <div className="">
+            <div className="flex flex-row justify-between items-start">
+              <Dialog.Title className="font-semibold text-2xl">
+                Deposit
+              </Dialog.Title>
+              <Dialog.Close>
+                <CloseIcon />
+              </Dialog.Close>
+            </div>
+            <div className="flex flex-col gap-6">
+              <DetailRow label="Routing" value="Standard Speed" />
+              <DetailRow
+                label="Amount of deposit"
+                value={`${formatEther(amount)} ${
+                  rollupChain.nativeCurrency.symbol
+                }`}
+              />
+              <DetailRow
+                label="Gas fee"
+                value={`${formatEther(gasEstimate)} ${
+                  parentChain.nativeCurrency.symbol
+                }`}
+              />
+              <DetailRow label="Time to transfer" value="~1 minute" />
+            </div>
+          </div>
+
           <div className="flex flex-row gap-2">
             <Dialog.Close asChild>
               <button className={buttonStyle}>Cancel</button>
